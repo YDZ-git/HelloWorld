df6d121e931f0a0a2ad63dd8dbab1695d1d06acf
YDZzzz <603683022@qq.com>
2025-10-15 22:01:36 +0800

    给main函数添加注释

--- main.cpp
+++ main.cpp
@@ -6,20 +6,21 @@ void sort(int arr[],int size)
     int temp = 0;
     for(int i = 0; i < size-1; i++)
     {
-    	for(int j =0; j < size-i-1; j++ )
-	{
-		if(arr[j] > arr[j+1])
-		{
-			temp = arr[j];
-			arr[j] = arr[j+1];
-			arr[j+1] = temp;
-		}
-	}
+        for(int j =0; j < size-i-1; j++ )
+        {
+                if(arr[j] > arr[j+1])
+                {
+                        temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                }
+        }
     }
 }
 
 int main()
 {
+	// 小张添加的注释
 	int arr[] = {56,12,63,5,89,12,54,123};
 	int size = sizeof(arr)/sizeof(arr[0]);
 	
