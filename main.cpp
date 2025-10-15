b201728851ef6484b1ca1cd4c854c56fa09098cf
YDZzzz <603683022@qq.com>
2025-10-15 19:37:49 +0800

    创建main.cpp文件,写了初始的代码

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void sort(int arr[],int size)
+{}
+
+int main()
+{
+	int arr[] = {56,12,63,5,89,12,54,123};
+	int size = sizeof(arr)/sizeof(arr[0]);
+	
+	sort(arr,size);
+	return 0;
+}
