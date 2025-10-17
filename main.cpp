4061c002bb168947bbacb8da09586dee1b3b13c8
YDZzzz <603683022@qq.com>
2025-10-17 19:29:06 +0800

    修改sort算法为快排算法

--- main.cpp
+++ main.cpp
@@ -1,26 +1,40 @@
 #include<iostream>
 using namespace std;
+
+void sort(int arr[],int i,int j)
+{
+	int val = arr[i];
+	int l = i;
+	int r = j;
+	while(l < r)
+	{
+		while(l<r&&arr[r]>=val)
+		{
+			r--;
+		}
+		if(l < r)
+		{
+			arr[l++] = arr[r];
+		}
+		while(l < r && arr[l]<=val)
+		{
+			l++;
+		}
+		if(l<r)
+		{
+			arr[r--] = arr[l];
+		}
+	}
+	sort[l] = val;
+	sort(arr,i,l-1);
+	sort(arr,l+1,j);
+}
+
+
 //YDZ给sort函数添加注释
 void sort(int arr[],int size)
 {
-    int temp = 0;
-    bool flag - false;
-    for(int i = 0; i < size-1; i++)
-    {
-        for(int j =0; j < size-i-1; j++ )
-        {
-			    // YDZ修改从大到小排序
-                if(arr[j] < arr[j+1])
-                {
-                        temp = arr[j];
-                        arr[j] = arr[j+1];
-                        arr[j+1] = temp;
-			flag = true;
-                }
-        }
-	if(!flag)
-		break;
-    }
+    sort(arr,0,size);
 }
 
 int main()
@@ -28,7 +42,11 @@ int main()
 	// 小张添加的注释
 	int arr[] = {56,12,63,5,89,12,54,123};
 	int size = sizeof(arr)/sizeof(arr[0]);
-	
+	for(int i = 0; i < size; ++i)
+	{
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
 	sort(arr,size);
 	return 0;
 }
